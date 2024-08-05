1b066d827bf8f5b03afb3f10ba9585c72a42a5b3
Chris Kaiser <admin@bahmut.de>
2024-08-05 03:44:59 +0200

    Implemented acceptance algorithm and consequences for demand magic diplomatic play

--- script/get-content.sh
+++ script/get-content.sh
@@ -33,7 +33,6 @@ cultureCount="$(countContent 'common/cultures')"
 religionCount="$(countContent 'common/religions')"
 technologyCount="$(countContent 'common/technology/technologies')"
 combatUnitTypesCount="$(countContent 'common/combat_unit_types')"
-diplomaticPlaysCount="$(countContent 'common/diplomatic_plays')"
 diplomaticActionsCount="$(countContent 'common/diplomatic_actions')"
 
 # Localization
@@ -61,8 +60,6 @@ printf '\n'
 printf ' - %s new Good' "$goodCount"
 printf '\n'
 printf ' - %s new Combat Unit Types' "$combatUnitTypesCount"
-printf '\n'
-printf ' - %s new Diplomatic Play' "$diplomaticPlaysCount"
 printf '\n'
 printf ' - %s new Diplomatic Action' "$diplomaticActionsCount"
 printf '\n'
