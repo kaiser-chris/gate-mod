ad4f7111a67989dd761212712c9feaac69761107
Chris Kaiser <chris.kaiser@bahmut.de>
2025-10-05 22:04:22 +0200

    Fixed tiger issues

--- script/generate-dna.sh
+++ script/generate-dna.sh
@@ -10,6 +10,9 @@ printf '\xEF\xBB\xBF' > ./mod/common/dna_data/00_lord_palmerston.txt
 
 find ./mod/common/dna_data/ -name "*.txt" -type f -print0 | while read -d $'\0' filename
 do
-  # Add missing genes
-  sed -i "s/teeth_accessory.*\=.*\"normal_teeth\".*\"normal_teeth\".*/teeth_accessory={ \"normal_teeth\" 0 \"normal_teeth\" 0 }\n$inject_dna/g" "$filename"
+  # Ignore mod files
+  if [[ $filename != *"0_gate_"* ]]; then
+   # Add missing genes
+    sed -i "s/teeth_accessory.*\=.*\"normal_teeth\".*\"normal_teeth\".*/teeth_accessory={ \"normal_teeth\" 0 \"normal_teeth\" 0 }\n$inject_dna/g" "$filename"
+  fi
 done
